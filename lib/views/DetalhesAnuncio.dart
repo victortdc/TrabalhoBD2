c058bce9e31fa9ec5bbec7becc2cfc8a6f027b82
Victor Trindade de Carvalho <63310837+victortdc@users.noreply.github.com>
2022-07-01 12:51:37 -0300

    all code
    all code

---
+++ lib/views/DetalhesAnuncio.dart
@@ -0,0 +1,197 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/Anuncio.dart';
+import 'package:lojavirtual/main.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DetalhesAnuncio extends StatefulWidget {
+  Anuncio anuncio;
+  DetalhesAnuncio(this.anuncio);
+
+  Anuncio _anuncio;
+
+
+  @override
+  _DetalhesAnuncioState createState() => _DetalhesAnuncioState();
+}
+
+class _DetalhesAnuncioState extends State<DetalhesAnuncio> {
+  Anuncio _anuncio;
+  String _idUsuarioLogado;
+  FirebaseUser usuarioLogado;
+  List<String> itensMenu = ["Editar Anúncio"];
+
+  recuperaDadosUsuarioLogado() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseUser usuarioLogado = await auth.currentUser();
+    _idUsuarioLogado = usuarioLogado.uid;
+  }
+
+  var idAnuncio;
+  List<Widget> _getListaImagens() {
+    List<String> listaUrlImagens = _anuncio.fotos;
+    return listaUrlImagens.map((url) {
+      return Container(
+        height: 250,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(url), fit: BoxFit.fitWidth)),
+      );
+    }).toList();
+  }
+
+  _escolhaMenuItem(String itemEscolhido) {
+    switch (itemEscolhido) {
+      case "Editar Anúncio":
+        _removerAnuncio(_anuncio.id);
+        Navigator.pop;
+        Navigator.pushNamed(context, "/novo-anuncio");
+
+        break;
+
+    }
+  }
+
+  _ligarTelefone(String telefone) async {
+    if (await canLaunch("tel:$telefone")) {
+      await launch("tel:$telefone");
+    } else {
+      print("Não pode fazer a ligação");
+    }
+  }
+
+  _removerAnuncio(String idAnuncio) {
+    Firestore db = Firestore.instance;
+    db
+        .collection("meus_anuncios")
+        .document(_idUsuarioLogado)
+        .collection("anuncios")
+        .document(idAnuncio)
+        .delete()
+        .then((_) {
+      db.collection("anuncios").document(idAnuncio).delete();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _anuncio = widget.anuncio;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Anuncio"),
+        elevation: 0,
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: _escolhaMenuItem,
+            itemBuilder: (context) {
+              return itensMenu.map((String item) {
+                return PopupMenuItem<String>(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList();
+            },
+          )
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 250,
+                child: Carousel(
+                  images: _getListaImagens(),
+                  dotSize: 8,
+                  dotBgColor: Colors.transparent,
+                  dotColor: Colors.white,
+                  autoplay: false,
+                  dotIncreasedColor: temaPadrao.primaryColor,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "R\$ ${_anuncio.preco}",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: temaPadrao.primaryColor),
+                    ),
+                    Text(
+                      "${_anuncio.titulo}",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    Text(
+                      "Descrição",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${_anuncio.descricao}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    Text(
+                      "Contato",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 66),
+                      child: Text(
+                        "${_anuncio.telefone}",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: GestureDetector(
+              child: Container(
+                child: Text(
+                  "Ligar",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                padding: EdgeInsets.all(16),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: temaPadrao.primaryColor,
+                    borderRadius: BorderRadius.circular(30)),
+              ),
+              onTap: () {
+                _ligarTelefone(_anuncio.telefone);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
